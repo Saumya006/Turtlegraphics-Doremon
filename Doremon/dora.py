a0a66f4d1ca222823b74d8c034badabfedaee31f
Saumya Swati <83216629+Saumya006@users.noreply.github.com>
2021-08-25 11:01:06 -0700

    Add files via upload
    turtle module in python provides to draw any types of shapes and designs or animations. Here I have created the famous cartoon character Doremon using turtle module.

---
+++ Doremon/dora.py
@@ -0,0 +1,20 @@
+from turtle import *
+from doraemon import *
+wn = Screen()
+
+pensize(3)
+speed(10)
+head()
+belt()
+face()
+nose()
+mouth()
+whisker()
+body()
+arms()
+pocket()
+bell()
+black_eyes()
+hideturtle()
+
+done()
